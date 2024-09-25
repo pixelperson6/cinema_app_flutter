a698242232423e4d2d8c2af9db2544dd17cbc966
Pixel <pixelperson6@gmail.com>
2024-09-25 08:31:49 +0545

    add : movie view

--- lib/app/core/values/s_text_strings.dart
+++ lib/app/core/values/s_text_strings.dart
@@ -1,4 +1,5 @@
 class STextString {
   STextString._();
 
+
 }
--- lib/app/modules/home/views/subview/movie_view.dart
+++ lib/app/modules/home/views/subview/movie_view.dart
@@ -1,3 +1,5 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:cinema_app/app/core/values/s_spacing.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
@@ -7,16 +9,80 @@ class MovieView extends GetView {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('MovieView'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'MovieView is working',
-          style: TextStyle(fontSize: 20),
+        body: Container(
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+
+            
+            CarouselSlider(
+              options: CarouselOptions(height: 400.0 , autoPlay: true),
+              items: [1, 2, 3, 4, 5].map((i) {
+                return Builder(
+                  builder: (BuildContext context) {
+                    return Container(
+                        width: MediaQuery.of(context).size.width,
+                        margin: EdgeInsets.symmetric(horizontal: 5.0),
+                        decoration: BoxDecoration(color: Colors.amber),
+                        child: Text(
+                          'text $i',
+                          style: TextStyle(fontSize: 16.0),
+                        ));
+                  },
+                );
+              }).toList(),
+            ),
+
+
+
+
+            Row(
+              children: [
+                SSpacing.smW,
+                Text(
+                  'Popular movies',
+                  style: Get.textTheme.titleLarge,
+                ),
+                const Spacer(),
+                Text(
+                  'see all',
+                  style: Get.textTheme.bodyMedium,
+                ),
+                Icon(
+                  Icons.arrow_forward_ios,
+                  size: 16,
+                ),
+                SSpacing.smW,
+              ],
+            ),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  Container(
+                    width: 150,
+                    height: 200,
+                    color: Colors.red,
+                  ),
+                  SSpacing.smW,
+                  Container(
+                    width: 150,
+                    height: 200,
+                    color: Colors.red,
+                  ),
+                  SSpacing.smW,
+                  Container(
+                    width: 150,
+                    height: 200,
+                    color: Colors.red,
+                  ),
+                  SSpacing.smW,
+                ],
+              ),
+            )
+          ],
         ),
       ),
-    );
+    ));
   }
 }
